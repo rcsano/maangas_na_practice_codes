c69383314f2072f0254363bf29894e99dcae4fa8
unknown <rc.sanoiz@gmail.com>
2026-03-09 17:26:30 +0800

    inserting the code and checking if it works

--- two_numbers_prints_bigger.py
+++ two_numbers_prints_bigger.py
@@ -1,4 +1,8 @@
-# Get the two numbers
-
-# Compare the numbers
-
+# Get the two numbers (in float)
+num_1 = float(input("Enter the first number: "))
+num_2 = float(input("Enter the second number: "))
+# Compare the numbers (if else)
+if num_1 > num_2:
+    print(num_1)
+else:
+    print(num_2)
