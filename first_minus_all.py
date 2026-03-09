b461e42dcfca50db7b20820d9e9db2cc2c47f127
unknown <rc.sanoiz@gmail.com>
2026-03-10 00:18:15 +0800

    final code hehe

--- first_minus_all.py
+++ first_minus_all.py
@@ -1,7 +1,6 @@
-# Ask for the first number
 result = float(input("Enter first number: "))
-# Ask for the remaining numbers and subtract it to the first
+
 for i in range(2, 11):
     result -= float(input(f"Enter number {i+1}: "))
-# Print
+
 print(result)
