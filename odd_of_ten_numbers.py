a47ecf69c2b363bfb9616156aee72580eb26cc98
unknown <rc.sanoiz@gmail.com>
2026-03-09 18:30:57 +0800

    final code, changed variable counter to odd_count

--- odd_of_ten_numbers.py
+++ odd_of_ten_numbers.py
@@ -1,9 +1,8 @@
-counter = 0
+odd_count = 0
+
 for i in range (10) :
-# Input
     num = float(input(f"Enter number {i+1}: "))
-# Check if odd
     if num % 2 != 0 :
-        counter += 1
-# Print
-print(counter)
+        odd_count += 1
+
+print(odd_count)
