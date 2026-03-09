14cb58ccdaa09f58c2fa8f9817fee4a0a119c3a8
unknown <rc.sanoiz@gmail.com>
2026-03-09 18:40:04 +0800

    final final hehe

--- two_numbers_with_equal_added.py
+++ two_numbers_with_equal_added.py
@@ -1,9 +1,9 @@
 num_1 = float(input("Enter the first number: "))
 num_2 = float(input("Enter the second number: "))
 
-if num_1 > num_2 :
+if num_1 > num_2:
     print(num_1)
-elif num_1 == num_2 :
+elif num_1 == num_2:
     print("Equal")
 else:
     print(num_2)
