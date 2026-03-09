f924796af0412110339d811d85941c778e8e25b1
unknown <rc.sanoiz@gmail.com>
2026-03-09 23:25:41 +0800

    final code

--- two_numbers_not_equal_added.py
+++ two_numbers_not_equal_added.py
@@ -1,10 +1,8 @@
 num_1 = float(input("Enter the first number: "))
 num_2 = float(input("Enter the second number: "))
 
-if num_1 < num_2:
-    print(num_1)
-elif num_1 != num_2:
+if num_1 != num_2:
     print("Not equal")
 else:
-    print(num_2)
+    print("Equal")
 
