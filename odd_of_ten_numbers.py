602b9559ea3095d90e72c14c441ba0589af90d44
unknown <rc.sanoiz@gmail.com>
2026-03-09 18:21:09 +0800

    inserted code

--- odd_of_ten_numbers.py
+++ odd_of_ten_numbers.py
@@ -1,5 +1,9 @@
+counter = 0
+for i in range (10) :
 # Input
-
+    num = float(input(f"Enter number {i+1}: "))
 # Check if odd
-
+    if num % 2 != 0 :
+        counter += 1
 # Print
+print(counter)
