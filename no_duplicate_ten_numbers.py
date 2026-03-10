8a975b276c11fd918548a2eafae96d788a168f38
unknown <rc.sanoiz@gmail.com>
2026-03-10 23:44:54 +0800

    added the codes

--- no_duplicate_ten_numbers.py
+++ no_duplicate_ten_numbers.py
@@ -1,2 +1,8 @@
+numbers = []
 # input 10 numbers
+for i in range(10):
+    numbers.append(float(input(f"Enter number {i+1}: ")))
 # print numbers without duplicates
+for num in numbers:
+    if numbers.count(num) == 1:
+        print(num)
