9950851889b83fd4f9560a1b55a5f123c751b161
unknown <rc.sanoiz@gmail.com>
2026-03-11 00:03:13 +0800

    changed the flow of the code, added the num shown and separates the num in num append

--- one_duplicate_ten_numbers.py
+++ one_duplicate_ten_numbers.py
@@ -1,8 +1,11 @@
 numbers = []
+num_shown = []
 
 for i in range(10):
-    numbers.append(float(input(f"Enter number {i+1}: ")))
+    num = float(input(f"Enter number {i+1}: "))
+    numbers.append(num)
 
 for num in numbers:
-    if numbers.count(num) == 1:
+    if num not in num_shown:
         print(num)
+        num_shown.append(num)
