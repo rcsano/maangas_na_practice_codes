c42e4c4569de65f11d4bc5d741b2c9b1e6f4b85b
unknown <rc.sanoiz@gmail.com>
2026-03-10 23:56:41 +0800

    final final code hehe. added variable num to make it clear

--- no_duplicate_ten_numbers.py
+++ no_duplicate_ten_numbers.py
@@ -1,7 +1,8 @@
 numbers = []
 
 for i in range(10):
-    numbers.append(float(input(f"Enter number {i+1}: ")))
+    num = float(input(f"Enter number {i+1}: "))
+    numbers.append(num)
 
 for num in numbers:
     if numbers.count(num) == 1:
