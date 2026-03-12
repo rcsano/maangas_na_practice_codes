b0feb15a95faa9b133a08af3c3338243ad072b6d
unknown <rc.sanoiz@gmail.com>
2026-03-12 22:19:32 +0800

    added the code

--- highest_number_until_invalid.py
+++ highest_number_until_invalid.py
@@ -1,7 +1,16 @@
 # Empty list
+numbers = []
 
 # Loop
+while True:
     # Enter number
+    try:
+        num = float(input("Number: "))
     # Stop if invalid
+    except ValueError:
+        break
+    numbers.append(num)
 
 # Show highest
+if numbers:
+    print("Highest number:", max(numbers))
