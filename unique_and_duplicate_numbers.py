bd60ae6d71ee7d7ccb21cb2680b9c9d616eed3d0
unknown <rc.sanoiz@gmail.com>
2026-03-12 17:53:23 +0800

    final code, did not remove the outline hehe

--- unique_and_duplicate_numbers.py
+++ unique_and_duplicate_numbers.py
@@ -1,9 +1,20 @@
 # Empty list
+numbers = []
 # Loop
+while True:
+    try:
     # Enter number
+        num = float(input("Enter a number: "))
+    except:
     # Stop if invalid
+        break
 
 # Check if number exists on list
-    # Appeared
+    if num in numbers:
+        # Appeared
+        print("Duplicate")
+    else:
     # New
+        print("Unique")
     # Store
+        numbers.append(num)
