3bc68188e74484f81b2dbd7fd47667b00cf724c0
unknown <rc.sanoiz@gmail.com>
2026-03-12 22:24:36 +0800

    final code added

--- average_until_invalid.py
+++ average_until_invalid.py
@@ -1,7 +1,16 @@
 # Empty list
+numbers = []
 
 # Loop
+while True:
     # Enter number
+    try:
+        num = float(input("Enter a number: "))
     # Stop if invalid
+    except ValueError:
+        break
+    numbers.append(num)
 
 # Check the average and display
+if numbers:
+    print("Average:", sum(numbers)/len(numbers))
