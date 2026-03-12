ea6b8af00d94477d55baff339de3eeec8ccb8d21
unknown <rc.sanoiz@gmail.com>
2026-03-12 18:31:45 +0800

    final code, did not remove the outline

--- sort_until_invalid.py
+++ sort_until_invalid.py
@@ -1,9 +1,18 @@
 # Empty list
+numbers = []
 
 # Loop
+while True:
     # Enter number
+    try:
+        num = float(input("Enter a number: "))
     # Stop if invalid
+    except ValueError:
+        break
+    numbers.append(num)
 
 # Sort lowest to highest
+numbers.sort()
 
 # Display sorted
+print("Numbers from lowest to highest:", numbers)
