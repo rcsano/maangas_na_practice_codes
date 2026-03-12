9a8b242752cda5ddbdbd0c2b65b5af39186e5958
unknown <rc.sanoiz@gmail.com>
2026-03-12 18:15:14 +0800

    final code, did not remove the outline

--- numbers_until_invalid.py
+++ numbers_until_invalid.py
@@ -1,7 +1,16 @@
 # Empty list
+numbers = []
 
 # Loop until invalid
+while True:
     # Enter number
+    try:
+        num = float(input("Enter a number: "))
     # Stop if invalid
+    except ValueError:
+        break
+    numbers.append(num)
 
 # Show lowest
+if numbers:
+    print("Lowest number:", min(numbers))
