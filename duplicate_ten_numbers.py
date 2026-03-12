8ea70b667ce40c8de21655f225666e8980b20abf
unknown <rc.sanoiz@gmail.com>
2026-03-12 19:01:53 +0800

    final code

--- duplicate_ten_numbers.py
+++ duplicate_ten_numbers.py
@@ -1,7 +1,16 @@
 # Empty list
+numbers = []
 
 # Input 10 numbers
+for i in range(10):
+    num = float(input(f"Number {i+1}: "))
+    numbers.append(num)
 
 # Store duplicate
+shown = []
 
 # Print the duplicates
+for num in numbers:
+    if numbers.count(num) > 1 and num not in shown:
+        print(num)
+        shown.append(num)
