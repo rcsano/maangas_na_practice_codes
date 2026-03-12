3aa8da5a505fe0ac24ebb1d563f5a6ce7c4f6780
unknown <rc.sanoiz@gmail.com>
2026-03-12 22:12:42 +0800

    added the code

--- highest_to_lowest_sort_until_invalid.py
+++ highest_to_lowest_sort_until_invalid.py
@@ -1,9 +1,18 @@
 # Empty list
+numbers = []
 
 # Loop
+while True:
     # Enter number
+    try:
+        num = float(input("Enter a number: "))
     # Stop if invalid
+    except ValueError:
+        break
+    numbers.append(num)
 
 # Sort highest to lowest
+numbers.sort(reverse=True)
 
 # Display sorted
+print("Numbers from highest to lowest:", numbers)
