6d356e6f06af6a8fe7c1077aa6ebdf7f5cfc3f62
unknown <rc.sanoiz@gmail.com>
2026-03-12 21:58:32 +0800

    final code

--- most_number_duplicate_until_invalid.py
+++ most_number_duplicate_until_invalid.py
@@ -1,8 +1,17 @@
 # Empty list
+numbers = []
 
 # Loop until invalid
+while True:
     # Enter number
+    try:
+        num = float(input("Enter a number: "))
     # Stop if invalid
+    except ValueError:
+        break
+    numbers.append(num)
 
 # Check duplicate
+most = max(numbers, key=numbers.count)
 # Print most duplicated number
+print("Number that has the most duplicates:", most)
