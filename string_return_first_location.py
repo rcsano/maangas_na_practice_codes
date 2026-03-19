51cd5f1b9f17279be61493c60000aeeacdff5f86
unknown <rc.sanoiz@gmail.com>
2026-03-20 01:55:09 +0800

    updated

--- string_return_first_location.py
+++ string_return_first_location.py
@@ -2,9 +2,9 @@ text = input("Enter string: ")
 sub_text = input("Enter substring: ")
 found_index = -1
 
-for index in range(len(text) - len(sub_text) + 1):
+for index in range(0, len(text) - len(sub_text) + 1):
     if text[index : index + len(sub_text)] == sub_text:
         found_index = index
         break
 
-print(found_index)
+print(found_index if found_index != -1 else "Substring not found")
