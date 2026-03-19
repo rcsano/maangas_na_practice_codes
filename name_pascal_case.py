20d44324f2db5ba280bc7791ce4ace0d0c019ed2
unknown <rc.sanoiz@gmail.com>
2026-03-19 18:17:09 +0800

    code

---
+++ name_pascal_case.py
@@ -0,0 +1,9 @@
+full_name = input("Enter your name: ")
+words = full_name.split()
+result = " "
+
+for word in words:
+    word = word.lower()
+    result += chr(ord(word[0]) - 32) + word[1:]
+
+print(result)
