fc32886f6eb488959538ea875f320bcf34d459ff
unknown <rc.sanoiz@gmail.com>
2026-03-19 18:24:46 +0800

    updated code, built-ins to make it easy to code hehe

--- name_snake_case.py
+++ name_snake_case.py
@@ -1,11 +1,2 @@
 full_name = input("Enter full name: ")
-words = full_name.split()
-result = " "
-
-for index in range(len(words)):
-    word = words[index].lower()
-    result += word
-    if index != len(words) - 1:
-        result += "_"
-
-print(result)
+print("_".join(full_name.lower().split()))
