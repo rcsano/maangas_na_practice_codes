62009f6a8ebe91406cadee3264a509d4621c95b2
unknown <rc.sanoiz@gmail.com>
2026-03-20 01:35:39 +0800

    added space to be consistent

--- add_space_characters_start.py
+++ add_space_characters_start.py
@@ -1,3 +1,3 @@
 text = input("Enter string: ")
 width = int(input("Total width: "))
-print(" "*(width - len(text)) + text if width > len(text) else text)
+print(" " * (width - len(text)) + text if width > len(text) else text)
