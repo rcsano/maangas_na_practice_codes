d00271530e188eed23cabfb9d1b70368d705a4e3
unknown <rc.sanoiz@gmail.com>
2026-03-20 00:35:53 +0800

    c to character, clearer variable

--- string_to_lower.py
+++ string_to_lower.py
@@ -1,2 +1,2 @@
 text = input("Enter string: ")
-print("".join(chr(ord(c)+32) if "A"<=c<="Z" else c for c in text))
+print("".join(chr(ord(character)+32) if "A"<=character<="Z" else character for character in text))
