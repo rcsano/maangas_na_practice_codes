1b8572c865754369a841fa58a3de98a3311d5bd7
unknown <rc.sanoiz@gmail.com>
2026-03-19 18:12:09 +0800

    shorten the code

--- name_in_reverse_casing.py
+++ name_in_reverse_casing.py
@@ -1,12 +1,2 @@
 full_name = input("Enter your full name: ")
-result = ""
-
-for character in full_name:
-    if character.islower():
-        result += character.upper()
-    elif character.isupper():
-        result += character.lower()
-    else:
-        result += character
-
-print(result)
+print(full_name.swapcase())
